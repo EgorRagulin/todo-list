7016b0708fdb37b970aa6c3a74d7f780c334d0d4
Egor Ragulin <egorr6885@gmail.com>
2020-06-12 20:40:22 +0300

    Add todo-item styling

--- src/todo/TodoItem.js
+++ src/todo/TodoItem.js
@@ -1,8 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types"
 
+const styles = {
+    li: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: '0.5rem 1rem',
+        border: "1px solid #ccc",
+        borderRadius: "4px",
+        marginBottom: "0.5rem"
+    },
+    input: {
+        marginRight: "1rem"
+    }
+}
+
 function TodoItem({ todoItem, index }) {
-    return <li><strong>{ index + 1 }</strong> {todoItem.title} | {todoItem.description}</li>
+    return (
+        <li style={styles.li}>
+            <span>
+                <input type="checkbox" style={styles.input}/>
+                <strong>{ index + 1 }</strong>
+                &nbsp;
+                {todoItem.title}
+                &nbsp;
+                {todoItem.description}
+            </span>
+
+            <button className={"remove-btn"}>&times;</button>
+        </li>
+    )
 }
 
 TodoItem.propTypes = {
